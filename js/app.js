131e41b72d40ef9dffb19530f217adba5f8ff141
syedikram1996dec@gmail.com <syedikram1996dec@gmail.com>
2020-12-30 23:32:05 +0500

    First Commit

---
+++ js/app.js
@@ -0,0 +1,161 @@
+$(document).ready(function()
+{
+    let hamberger = document.querySelector('.hamberger');
+    let times = document.querySelector('.times');
+    let mobileNav = document.querySelector('.mobile-nav');
+    let loadMore = document.querySelector('#loadMore');
+
+    var sfg = document.getElementById("sub-filter-gallery");
+    var sfgb = document.getElementById("sub-filter-gallery-blender");
+
+    hamberger.addEventListener('click',function()
+    {
+        mobileNav.classList.add('open');
+        hamberger.classList.add('hambergerimgdisappear');
+    });
+
+    times.addEventListener('click',function()
+    {
+        hamberger.classList.remove('hambergerimgdisappear');
+        mobileNav.classList.remove('open');
+
+    });
+
+    //main filter gallery
+    $('.list').click(function()
+    {
+        const value = $(this).attr('data-filter');
+        if (value == 'all') 
+        {
+            $('.itemBox').show(200);
+            loadMore.style.visibility = "visible";
+             //Load More Button
+    
+        $(".loadMoreAll").slice(3,11).hide();
+        $("#loadMore").on('click', function (e) {
+            e.preventDefault();
+            $(".loadMoreAll:hidden").slice(0, 3).slideDown(1000);
+        });
+         
+        }
+        else
+        {
+            loadMore.style.visibility = "hidden";
+            $('.itemBox').not('.' + value).hide(100);
+            $('.itemBox').filter('.' + value).show(200);
+        }
+
+        if (value == "unity") 
+        {
+            sfg.style.display = "block";
+            sfgb.style.display = "none";
+            $('#sub-filter-gallery-blender ul li:nth-child(1)').addClass('active').siblings().removeClass('active');
+        }
+
+        else if (value == "blender") 
+        {
+            sfgb.style.display = "block";
+            sfg.style.display = "none";
+            $('#sub-filter-gallery ul li:nth-child(1)').addClass('active').siblings().removeClass('active');
+        }
+        else
+        {
+            sfg.style.display = "none";
+            sfgb.style.display = "none";
+            $('#sub-filter-gallery ul li:nth-child(1)').addClass('active').siblings().removeClass('active');
+            $('#sub-filter-gallery-blender ul li:nth-child(1)').addClass('active').siblings().removeClass('active');
+
+        }
+
+    });
+
+    // //sub filter gallery unity
+    // $('.sub_list').click(function()
+    // {
+    //     const value = $(this).attr('data-filter');
+    //     if (value == 'allgames') 
+    //     {
+    //         $('.games').show(200);
+    //     }
+    //     else
+    //     {
+    //         $('.games').not('.' + value).hide(100);
+    //         $('.games').filter('.' + value).show(200);
+    //     }
+    // });
+
+    //sub filter gallery unity
+    $('.sub_list').click(function()
+    {
+        const value = $(this).attr('data-filter');
+        if (value == 'allunity') 
+        {
+            $('.unity').show(200);
+        }
+        else
+        {
+            $('.unity').not('.' + value).hide(100);
+            $('.unity').filter('.' + value).show(200);
+        }
+    });
+
+    // //sub filter gallery blender
+    // $('.sub_list_blender').click(function()
+    // {
+    //     const value = $(this).attr('data-filter');
+    //     if (value == 'allmodel') 
+    //     {
+    //         $('.model').show(200);
+    //     }
+    //     else
+    //     {
+    //         $('.model').not('.' + value).hide(100);
+    //         $('.model').filter('.' + value).show(200);
+    //     }
+    // });
+
+    //sub filter gallery blender
+    $('.sub_list_blender').click(function()
+    {
+        const value = $(this).attr('data-filter');
+        if (value == 'allblender') 
+        {
+            $('.blender').show(200);
+        }
+        else
+        {
+            $('.blender').not('.' + value).hide(100);
+            $('.blender').filter('.' + value).show(200);
+        }
+    });
+
+    //Active main filter simbling
+    $('.list').click(function()
+    {
+        $(this).addClass('active').siblings().removeClass('active');
+    });
+
+    //Active sub filter simbling unity
+    $('.sub_list').click(function()
+    {
+        $(this).addClass('active').siblings().removeClass('active');
+    });
+
+    //Active sub filter simbling blender
+    $('.sub_list_blender').click(function()
+    {
+        $(this).addClass('active').siblings().removeClass('active');
+    });
+
+
+
+    //Load More Button
+    
+        $(".loadMoreAll").slice(3,11).hide();
+        $("#loadMore").on('click', function (e) {
+            e.preventDefault();
+            $(".loadMoreAll:hidden").slice(0, 3).slideDown(10);
+        });
+
+});
+
---
+++ js/modal.js
@@ -0,0 +1,122 @@
+//the modal
+var modal = document.getElementById("myModal");
+
+//the image that showed on model
+var modalImg = document.getElementById("modal-image");
+
+//the image we click on
+var img = document.getElementById("myImg");
+var img2 = document.getElementById("myImg2");
+var img3 = document.getElementById("myImg3");
+var img4 = document.getElementById("myImg4");
+var img5 = document.getElementById("myImg5");
+var img6 = document.getElementById("myImg6");
+var img7 = document.getElementById("myImg7");
+var img8 = document.getElementById("myImg8");
+var img9 = document.getElementById("myImg9");
+var img10 = document.getElementById("myImg10");
+var img11 = document.getElementById("myImg11");  
+
+//the image we want to show on modal
+var imgfinal = document.getElementById("myfinalImg");
+var imgfinal2 = document.getElementById("myfinalImg2");
+var imgfinal3 = document.getElementById("myfinalImg3");
+var imgfinal4 = document.getElementById("myfinalImg4");
+var imgfinal5 = document.getElementById("myfinalImg5");
+var imgfinal6 = document.getElementById("myfinalImg6");
+var imgfinal7 = document.getElementById("myfinalImg7");
+var imgfinal8 = document.getElementById("myfinalImg8");
+var imgfinal9 = document.getElementById("myfinalImg9");
+var imgfinal10 = document.getElementById("myfinalImg10");
+var imgfinal11 = document.getElementById("myfinalImg11");
+
+//caption text
+var captionText = document.getElementById("caption");
+
+//close modal
+var span = document.getElementsByClassName("close")[0];
+
+
+img.onclick = function()
+{
+    modal.style.display="block";
+    modalImg.src = imgfinal.src;
+    captionText.innerHTML = this.alt;
+}
+
+img2.onclick = function()
+{
+    modal.style.display="block";
+    modalImg.src = imgfinal2.src;
+    captionText.innerHTML = this.alt;
+}
+
+img3.onclick = function()
+{
+    modal.style.display="block";
+    modalImg.src = imgfinal3.src;
+    captionText.innerHTML = this.alt;
+}
+
+img4.onclick = function()
+{
+    modal.style.display="block";
+    modalImg.src = imgfinal4.src;
+    captionText.innerHTML = this.alt;
+}
+
+img5.onclick = function()
+{
+    modal.style.display="block";
+    modalImg.src = imgfinal5.src;
+    captionText.innerHTML = this.alt;
+}
+
+img6.onclick = function()
+{
+    modal.style.display="block";
+    modalImg.src = imgfinal6.src;
+    captionText.innerHTML = this.alt;
+}
+
+img7.onclick = function()
+{
+    modal.style.display="block";
+    modalImg.src = imgfinal7.src;
+    captionText.innerHTML = this.alt;
+}
+
+img8.onclick = function()
+{
+    modal.style.display="block";
+    modalImg.src = imgfinal8.src;
+    captionText.innerHTML = this.alt;
+}
+
+img9.onclick = function()
+{
+    modal.style.display="block";
+    modalImg.src = imgfinal9.src;
+    captionText.innerHTML = this.alt;
+}
+
+img10.onclick = function()
+{
+    modal.style.display="block";
+    modalImg.src = imgfinal10.src;
+    captionText.innerHTML = this.alt;
+}
+
+img11.onclick = function()
+{
+    modal.style.display="block";
+    modalImg.src = imgfinal11.src;
+    captionText.innerHTML = this.alt;
+}
+
+
+
+span.onclick = function()
+{
+    modal.style.display = "none";
+}
